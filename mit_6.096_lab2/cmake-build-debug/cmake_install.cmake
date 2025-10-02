6c3d852b49caaaf500bd79d3c88a3079de79c203
yard <106509725+a-hiremath@users.noreply.github.com>
2025-10-02 10:29:35 -0700

    updated for editor

--- mit_6.096_lab2/cmake-build-debug/cmake_install.cmake
+++ mit_6.096_lab2/cmake-build-debug/cmake_install.cmake
@@ -1,4 +1,4 @@
-# Install script for directory: /Users/adihiremath/CLionProjects/mit_6.096_lab2
+# Install script for directory: /Users/adihiremath/Documents/GitHub/6.096/mit_6.096_lab2
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
@@ -40,7 +40,7 @@ endif()
 string(REPLACE ";" "\n" CMAKE_INSTALL_MANIFEST_CONTENT
        "${CMAKE_INSTALL_MANIFEST_FILES}")
 if(CMAKE_INSTALL_LOCAL_ONLY)
-  file(WRITE "/Users/adihiremath/CLionProjects/mit_6.096_lab2/cmake-build-debug/install_local_manifest.txt"
+  file(WRITE "/Users/adihiremath/Documents/GitHub/6.096/mit_6.096_lab2/cmake-build-debug/install_local_manifest.txt"
      "${CMAKE_INSTALL_MANIFEST_CONTENT}")
 endif()
 if(CMAKE_INSTALL_COMPONENT)
@@ -56,6 +56,6 @@ else()
 endif()
 
 if(NOT CMAKE_INSTALL_LOCAL_ONLY)
-  file(WRITE "/Users/adihiremath/CLionProjects/mit_6.096_lab2/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
+  file(WRITE "/Users/adihiremath/Documents/GitHub/6.096/mit_6.096_lab2/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
      "${CMAKE_INSTALL_MANIFEST_CONTENT}")
 endif()
